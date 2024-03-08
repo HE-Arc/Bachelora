7d68ef7e82523ee5e944c798a3ed8a5f13c7711d
Miranda Fleury <miranda.fleury@he-arc.ch>
2024-03-08 13:34:13 +0100

    DONE Install Quasar Framework

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,8 +3,11 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { Quasar } from 'quasar'
+import quasarUserOptions from './quasar-user-options'
+import "quasar/dist/quasar.sass"
 
-const app = createApp(App)
+const app = createApp(App).use(Quasar, quasarUserOptions)
 
 app.use(router)
 
