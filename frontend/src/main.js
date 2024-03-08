949307e69622b3d4fc70231bc09c2fd691946944
ClawdeenFleury <52837025+ClawdeenFleury@users.noreply.github.com>
2024-03-08 13:57:14 +0100

    Revert "DONE Install Quasar Framework"
    This reverts commit 3f67e13d7e49fd92e09a7c68cd0cf63448c879c7.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,11 +3,8 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import { Quasar } from 'quasar'
-import quasarUserOptions from './quasar-user-options'
-import "quasar/dist/quasar.sass"
 
-const app = createApp(App).use(Quasar, quasarUserOptions)
+const app = createApp(App)
 
 app.use(router)
 
