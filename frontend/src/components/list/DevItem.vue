6e1ffdfe8384c122905c1940394034d400e90c0b
Miranda Fleury <miranda.fleury@he-arc.ch>
2024-03-14 22:02:44 +0100

    DONE Create home page

---
+++ frontend/src/components/list/DevItem.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+const props = defineProps({
+  name: {
+    type: String,
+    required: true,
+  }
+});
+
+</script>
+
+<template>
+  <q-item>
+    <q-item-section avatar>
+      <q-avatar color="secondary" text-color="white" icon="code" />
+    </q-item-section>
+    <q-item-section>{{ props.name }}</q-item-section>
+  </q-item>
+</template>
+
+<style scoped>
+
+</style>
