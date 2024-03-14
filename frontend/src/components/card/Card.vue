7785bf2c36ab740992abe8e57f434abfb0c3d6cc
Miranda Fleury <miranda.fleury@he-arc.ch>
2024-03-15 00:08:26 +0100

    WIP Create list bachelors page

---
+++ frontend/src/components/card/Card.vue
@@ -0,0 +1,38 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="q-pa-md row items-start q-gutter-md">
+    <q-card :key="bachelor" class="my-card" flat bordered >
+      <q-card-section horizontal>
+        <q-card-section class="q-pt-xs">
+          <div class="text-overline">Proposée par entreprise</div>
+          <div class="text-h5 q-mt-sm q-mb-xs">Titre</div>
+          <div class="text-caption text-grey">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </div>
+
+          <div class="text-overline">Enseignat ayant entré le travail</div>
+
+          <div>
+            <q-chip v-for="item in 5" color="secondary" :key="item" text-color="white">
+              IA
+            </q-chip>
+          </div>
+        </q-card-section>
+      </q-card-section>
+
+      <q-separator />
+
+      <q-card-actions align="around">
+        <q-btn flat icon="edit" color="primary"/>
+        <q-btn flat round icon="delete" color="red" />
+      </q-card-actions>
+    </q-card>
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
---
+++ frontend/src/views/BachelorsView.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref } from 'vue'
+
+import Title from "@/components/Title.vue";
+import Card from "@/components/card/Card.vue";
+import SecondaryButton from "@/components/SecondaryButton.vue";
+
+const currentPage = ref(1);
+
+</script>
+
+<template>
+  <section>
+    <Title text="Liste des travaux de bachelors"/>
+
+    <SecondaryButton text="Ajouter un travail de bachelor"/>
+
+    <Card v-for="card in 10" :key="card" />
+
+    <div class="q-pa-lg flex flex-center">
+      <q-pagination color="secondary"
+        v-model="currentPage"
+        :max="5"
+        direction-links
+      />
+    </div>
+
+  </section>
+</template>
+
+<style scoped lang="scss">
+
+</style>
