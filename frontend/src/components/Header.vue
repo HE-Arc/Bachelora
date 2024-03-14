ab5adb5077fbb0dbb6062281d8494663d9d4a68b
Miranda Fleury <miranda.fleury@he-arc.ch>
2024-03-14 18:29:48 +0100

    DONE Create navbar

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,100 @@
+<script setup>
+  import {ref} from "vue";
+
+  const leftDrawerOpen = ref(null)
+  const menuList = [
+    {
+      icon: 'home',
+      name: 'home',
+      label: 'Accueil',
+      separator: true
+    },
+    {
+      icon: 'school',
+      name: 'list-bachelors',
+      label: 'Liste des travaux de bachelor',
+      separator: false
+    },
+    {
+      icon: 'star',
+      name: 'chosen-bachelors',
+      label: 'Ma sélection',
+      separator: true
+    },
+    {
+      icon: 'person',
+      name: 'account',
+      label: 'Mon compte',
+      separator: false
+    },
+    {
+      icon: 'logout',
+      name: 'logout',
+      label: 'Déconnexion',
+      separator: false
+    },
+  ];
+</script>
+
+<template>
+  <q-header elevated class="header">
+    <q-toolbar>
+      <q-btn
+      flat
+      dense
+      round
+      @click="leftDrawerOpen = !leftDrawerOpen"
+      aria-label="Menu"
+      icon="menu"
+      />
+      <q-toolbar-title class="title ubuntu-bold">
+        Bachelora
+      </q-toolbar-title>
+    </q-toolbar>
+  </q-header>
+
+  <q-drawer
+  v-model="leftDrawerOpen"
+  show-if-above
+  :breakpoint="500"
+  bordered
+  >
+    <q-scroll-area class="fit">
+      <q-list>
+        <q-item-label header>Bachelora</q-item-label>
+
+        <template v-for="(menuItem, index) in menuList" :key="index">
+          <router-link :to="{ name: menuItem.name }">
+            <q-item clickable :active="$route.name === menuItem.name" :class="{ active : $route.name === menuItem.name }" v-ripple>
+              <q-item-section avatar>
+                <q-icon :name="menuItem.icon"></q-icon>
+              </q-item-section>
+
+              <q-item-section>
+                {{ menuItem.label }}
+              </q-item-section>
+            </q-item>
+          </router-link>
+
+          <q-separator :key="'sep' + index"  v-if="menuItem.separator"></q-separator>
+        </template>
+      </q-list>
+    </q-scroll-area>
+  </q-drawer>
+
+</template>
+
+<style scoped lang="scss">
+  .header {
+    background-color: $secondary;
+    text-align: center;
+
+    .title {
+      font-weight: bold;
+    }
+  }
+
+  .active {
+    background-color: aliceblue;
+  }
+</style>
--- frontend/src/quasar-user-options.js
+++ frontend/src/quasar-user-options.js
@@ -4,7 +4,21 @@ import '@quasar/extras/material-icons/material-icons.css'
 
 // To be used on app.use(Quasar, { ... })
 export default {
-  config: {},
+  config: {
+      brand: {
+          primary: '#056e83',
+          secondary: '#f47967',
+          accent: '#9C27B0',
+
+          dark: '#1d1d1d',
+          'dark-page': '#121212',
+
+          positive: '#21BA45',
+          negative: '#C10015',
+          info: '#31CCEC',
+          warning: '#F2C037',
+      }
+  },
   plugins: {
   }
 }
