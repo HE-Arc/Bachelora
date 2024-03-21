27c895716cc7cf45755ccfde5f46ecd35579b4da
Oguey Stéphane <stephane.oguey@he-arc.ch>
2024-03-21 14:34:18 +0100

    Add Orientation urls

--- api/bacheloraapp/urls.py
+++ api/bacheloraapp/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 router = DefaultRouter()
 router.register(r'bachelor', views.BachelorViewSet)
+router.register(r'orientation', views.OrientationViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
