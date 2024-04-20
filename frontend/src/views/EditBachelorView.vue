6e51c8e0ad904240a27c20048b82587e2b734be5
Miranda Fleury <miranda.fleury@he-arc.ch>
2024-04-20 15:55:17 +0200

    WIP Reunite request in a class : DONE Edit Bachelor

--- frontend/src/views/EditBachelorView.vue
+++ frontend/src/views/EditBachelorView.vue
@@ -1,39 +1,18 @@
 <script setup>
+import { useRoute } from 'vue-router';
+import {computed, onMounted, ref} from 'vue';
 
 import Title from "@/components/Title.vue";
 import EditBachelorForm from "@/components/form/EditBachelorForm.vue";
-
-import { useRoute } from 'vue-router';
-import {computed, onMounted, ref} from 'vue';
-import axios from "axios";
 import BackButton from "@/components/BackButton.vue";
-
-const API_LINK = import.meta.env.VITE_API_LINK;
+import BackendRequest from "@/request/request.js";
 
 const route = useRoute();
 const id = computed(() => route.params.id);
 
 const bachelor = ref([]);
 const fetchBachelor = async () => {
-  try {
-    const bachelorData = await axios.get(`${API_LINK}api/bachelor/${id.value}/`);
-    bachelor.value = bachelorData.data;
-
-    bachelor.value.tagsItems = [];
-    for (const id of bachelor.value.tags) {
-      try {
-        const tags = await axios.get(`${API_LINK}api/tag/${id}/`);
-        bachelor.value.tagsItems.push(tags.data);
-      } catch (error) {
-        console.error(`Erreur lors de la récupération des tags : `, error);
-      }
-    }
-
-    const res = await axios.get(`${API_LINK}api/teacher/${bachelor.value.teacher}/`);
-    bachelor.value.teacher = res.data.id;
-  } catch (error) {
-    console.error(`Erreur lors de la récupération des tags : `, error);
-  }
+  bachelor.value = await BackendRequest.fetchBachelor(id);
 };
 
 onMounted(() => {
