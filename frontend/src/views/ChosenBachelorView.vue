6ec04b5b69fb1d20e054128353275a92a3a20e2e
Miranda Fleury <miranda.fleury@he-arc.ch>
2024-04-25 18:56:36 +0200

    Display selected bachelors and changer backbutton to go to the last page instead of given link

--- frontend/src/views/ChosenBachelorView.vue
+++ frontend/src/views/ChosenBachelorView.vue
@@ -1,9 +1,24 @@
 <script setup>
 
-import Title from "@/components/Title.vue";
-import BachelorCard from "@/components/card/BachelorCard.vue";
+  import Title from "@/components/Title.vue";
+  import BachelorCard from "@/components/card/BachelorCard.vue";
+  import BackendRequest from "@/request/request.js";
+  import {onMounted, ref} from "vue";
 
+  const choosen_bachelor = ref([]);
+  const fetchSelectBachelor = async() => {
+    // TODO Adding student id
+    const res = await BackendRequest.fetchAllBachelorsFromStudentById(4);
 
+    for (const id of res.data.bachelors) {
+      const bachelor = await BackendRequest.fetchBachelorById(id);
+      choosen_bachelor.value.push(bachelor);
+    }
+  };
+
+  onMounted(() => {
+    fetchSelectBachelor();
+  });
 
 </script>
 
@@ -13,13 +28,13 @@ import BachelorCard from "@/components/card/BachelorCard.vue";
         <Title text="Ma sélection" />
 
         <ul id="bachelors" class="bachelors">
-          <template v-if="displayBachelors.length === 0">
+          <template v-if="choosen_bachelor.length === 0">
             <p>Aucun résultat trouvé.</p>
           </template>
 
           <template v-else>
-            <li class="item-bachelor" v-for="bachelor in displayBachelors" :key="bachelor">
-              <BachelorCard :bachelor="bachelor" @get-item-delete-id="getItemDeleteId"/>
+            <li class="item-bachelor" v-for="bachelor in choosen_bachelor" :key="bachelor">
+              <BachelorCard :bachelor="bachelor" />
             </li>
           </template>
         </ul>
@@ -28,5 +43,15 @@ import BachelorCard from "@/components/card/BachelorCard.vue";
 </template>
 
 <style scoped lang="scss">
+  .bachelors {
+      margin-top: 2rem;
+      padding: 0;
+      list-style: none;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      gap: 2rem;
+    }
 
 </style>
